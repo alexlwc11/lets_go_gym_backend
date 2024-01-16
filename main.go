c15a8638d221f24cec9ab26512db51137b3e0424
Alex Lam <alexlwc.dev@gmail.com>
2024-01-16 16:14:53 +0000

    feat: refactor main.go

--- main.go
+++ main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"lets_go_gym_backend/apis"
 	"lets_go_gym_backend/configs"
+	LoadData "lets_go_gym_backend/load_data"
 	"lets_go_gym_backend/models"
 	"lets_go_gym_backend/repositories"
 
@@ -12,21 +13,82 @@ import (
 )
 
 func main() {
+	// init database connection
+	db := InitDatabase()
+
+	// init router
+	router := InitRouter(db)
+
+	router.Run("localhost:8080")
+}
+
+// Database
+func InitDatabase() *gorm.DB {
 	mConfig, err := configs.InitConfig()
 	if err != nil {
 		panic("Failed to read config")
 	}
 
-	db, err := gorm.Open(mysql.Open(mConfig.GetDSNString()), &gorm.Config{})
+	db, err := connectDatabase(mConfig)
 	if err != nil {
 		panic("Failed to connect database")
 	}
 
-	// Migrate the schema
+	// Migration
+	proceedSchemaMigration(db)
+
+	// Load default data if needed
+	loadDefaultDataIfNeeded(db)
+
+	return db
+}
+
+func connectDatabase(config *configs.Config) (*gorm.DB, error) {
+	db, err := gorm.Open(mysql.Open(config.GetDSNString()), &gorm.Config{})
+	if err != nil {
+		return nil, err
+	}
+
+	return db, nil
+}
+
+func proceedSchemaMigration(db *gorm.DB) {
 	db.AutoMigrate(&models.Region{})
 	db.AutoMigrate(&models.District{})
 	db.AutoMigrate(&models.SportsCenter{})
+}
 
+func loadDefaultDataIfNeeded(db *gorm.DB) {
+	if db.First(&models.Region{}) == nil {
+		defaultRegions, err := LoadData.LoadRegions("./data/regions.json")
+		if err != nil {
+			println("Failed to load default regions data")
+		}
+
+		db.Create(defaultRegions)
+	}
+
+	if db.First(&models.District{}) == nil {
+		defaultDistricts, err := LoadData.LoadRegions("./data/districts.json")
+		if err != nil {
+			println("Failed to load default districts data")
+		}
+
+		db.Create(defaultDistricts)
+	}
+
+	if db.First(&models.SportsCenter{}) == nil {
+		defaultSportsCenters, err := LoadData.LoadRegions("./data/sports_centers.json")
+		if err != nil {
+			println("Failed to load default sports centers data")
+		}
+
+		db.Create(defaultSportsCenters)
+	}
+}
+
+// Router
+func InitRouter(db *gorm.DB) *gin.Engine {
 	// region
 	regionRepo := repositories.NewRegionRepository(db)
 	regionHandler := apis.NewRegionHandler(regionRepo)
@@ -41,13 +103,25 @@ func main() {
 
 	router := gin.Default()
 	// region
-	router.GET("/regions", regionHandler.GetAllRegions)
+	setupRegionEndpoints(router, regionHandler)
 
 	// district
-	router.GET("/districts", districtHandler.GetAllDistricts)
+	setupDistrictEndpoints(router, districtHandler)
 
 	// sports center
-	router.GET("/sports_centers", sportsCenterHandler.GetAllSportsCenters)
+	setupSportsCenterEndpoints(router, sportsCenterHandler)
 
-	router.Run("localhost:8080")
+	return router
+}
+
+func setupRegionEndpoints(engine *gin.Engine, regionHandler *apis.RegionHandler) {
+	engine.GET("/regions", regionHandler.GetAllRegions)
+}
+
+func setupDistrictEndpoints(engine *gin.Engine, districtHandler *apis.DistrictHandler) {
+	engine.GET("/districts", districtHandler.GetAllDistricts)
+}
+
+func setupSportsCenterEndpoints(engine *gin.Engine, sportsCenterHandler *apis.SportsCenterHandler) {
+	engine.GET("/sports_centers", sportsCenterHandler.GetAllSportsCenters)
 }

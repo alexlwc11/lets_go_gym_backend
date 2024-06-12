ce81ed3c168353a6eaa3869df180f28202e35580
Alex Lam <alexlwc.dev@gmail.com>
2024-06-12 17:57:57 +0100

    wip

--- apis_test/user_bookmark_test.go
+++ apis_test/user_bookmark_test.go
@@ -1,37 +1,39 @@
-// package apis_test
+package apis_test
 
-// import (
-// 	"encoding/json"
-// 	"net/http"
-// 	"testing"
+import (
+	"encoding/json"
+	"net/http"
+	"testing"
 
-// 	"lets_go_gym_backend/apis"
-// 	MockRepo "lets_go_gym_backend/repositories_test"
-// 	RequestHelper "lets_go_gym_backend/test"
-// )
+	"lets_go_gym_backend/apis"
+	"lets_go_gym_backend/middleware"
+	MockRepo "lets_go_gym_backend/repositories_test"
+	RequestHelper "lets_go_gym_backend/test"
+)
 
-// // TODO look into Auth middleware
-// func TestUserBookmarkWithData(t *testing.T) {
-// 	mockUserBookmarkRepositoryWithSuccessResult := MockRepo.NewMockUserBookmarkRepositoryWithSuccessResult()
-// 	userBookmarkHandler := apis.NewUserBookmarkHandler(mockUserBookmarkRepositoryWithSuccessResult)
+// TODO look into Auth middleware
+func TestUserBookmarkWithData(t *testing.T) {
+	mockUserBookmarkRepositoryWithSuccessResult := MockRepo.NewMockUserBookmarkRepositoryWithSuccessResult()
+	sessionTokenRepo := MockRepo.NewMockSessionTokenRepository()
+	userBookmarkHandler := apis.NewUserBookmarkHandler(mockUserBookmarkRepositoryWithSuccessResult)
 
-// 	// TODO wip
-// 	t.Run("GetUserBookmarks_SuccessWithEmptyList", func(t *testing.T) {
-// 		statusCode, responseBody := RequestHelper.ServeHTTPRequest(http.MethodGet, "/", nil, userBookmarkHandler.RegisterRoutes)
+	// TODO wip
+	t.Run("GetUserBookmarks_SuccessWithEmptyList", func(t *testing.T) {
+		statusCode, responseBody := RequestHelper.ServeHTTPRequestWithMiddleware(http.MethodGet, "/", nil, userBookmarkHandler.RegisterRoutes, middleware.AuthRequired(sessionTokenRepo.FindByValue))
 
-// 		if statusCode != http.StatusOK {
-// 			t.Errorf("expected %d but received %d", http.StatusOK, statusCode)
-// 		}
+		if statusCode != http.StatusOK {
+			t.Errorf("expected %d but received %d", http.StatusOK, statusCode)
+		}
 
-// 		var data map[string]interface{}
-// 		err := json.Unmarshal(responseBody, &data)
-// 		if err != nil {
-// 			t.Error(err)
-// 			return
-// 		}
-// 	})
-// }
+		var data map[string]interface{}
+		err := json.Unmarshal(responseBody, &data)
+		if err != nil {
+			t.Error(err)
+			return
+		}
+	})
+}
 
-// func TestUserBookmarkWithoutData(t *testing.T) {
+func TestUserBookmarkWithoutData(t *testing.T) {
 
-// }
+}
--- test/request_helper.go
+++ test/request_helper.go
@@ -18,3 +18,16 @@ func ServeHTTPRequest(method string, target string, body io.Reader, registerRout
 
 	return rr.Code, rr.Body.Bytes()
 }
+
+func ServeHTTPRequestWithMiddleware(method string, target string, body io.Reader, registerRoutesFunc func(*gin.RouterGroup), middlewares ...gin.HandlerFunc) (int, []byte) {
+	req := httptest.NewRequest(method, target, body)
+	rr := httptest.NewRecorder()
+	router := gin.Default()
+
+	router.Use(middlewares...)
+	registerRoutesFunc(&router.RouterGroup)
+
+	router.ServeHTTP(rr, req)
+
+	return rr.Code, rr.Body.Bytes()
+}
